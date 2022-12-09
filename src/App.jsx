4e3e95491a16ccda311cd73368d0bdf10380b208
UsuaOSilver <anguyen8063@student.nocccd.edu>
2022-12-09 22:25:58 +0700

    check if user own a membership nft

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
-import { useAddress, ConnectWallet } from '@thirdweb-dev/react';
+import { useAddress, ConnectWallet, useContract, useNFTBalance } from '@thirdweb-dev/react';
+import { useState, useEffect, useMemo } from 'react';
 
 const App = () => {
   const address = useAddress();
-  console.log("Address:", address);
+  console.log("👋 Address:", address);
+  
+  //Initialize Edition Drop contract
+  const editionDropAddress = "0xFf423fC0149dEFACf4A51f752cb8F7d208FD4e09"
+  const { contract: editionDrop } = useContract(editionDropAddress, "edition-drop");
+  
+  // Hook to check if the user has the NFT
+  const { data: nftBalance } = useNFTBalance(editionDrop, address, "0")
+  
+  const hasClaimedNFT = useMemo(() => {
+    return nftBalance && nftBalance.gt(0)
+  }, [nftBalance])
   
   // Case 1: Wallet not connected
   if (!address) {
