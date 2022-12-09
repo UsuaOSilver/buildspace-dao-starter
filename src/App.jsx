ce06b9125f15a4bae110411122f99c58b7d2cb15
UsuaOSilver <anguyen8063@student.nocccd.edu>
2022-12-09 19:56:41 +0700

    Build welcome page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
+import { useAddress, ConnectWallet } from '@thirdweb-dev/react';
+
 const App = () => {
+  const address = useAddress();
+  console.log("Address:", address);
+  
+  // Case 1: Wallet not connected
+  if (!address) {
+    return (
+      <div className="landing">
+        <h1>Welcome to SaigonDAO</h1>
+        <div className='btn-hero'>
+          <ConnectWallet />
+        </div>
+      </div>
+    );
+  }
+  
+  // Case 2: Wallet has connected before
   return (
-    <div className="landing">
-      <h1>Welcome to My DAO</h1>
+    <div className='landing'>
+      <h1>wallet connected, now what!</h1>
     </div>
   );
 };
